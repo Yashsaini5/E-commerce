4246c8c8285915f8a32ed50f8e81ac54f9340bf2
Yash Saini <yashsaini0505@gmail.com>
2025-06-17 20:34:08 +0530

    updated main

--- shopping app/src/main.jsx
+++ shopping app/src/main.jsx
@@ -1,21 +1,15 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import DataProvider from "./context/DataProvider.jsx"
-import ReactDOM from "react-dom"
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-// createRoot(document.getElementById('root')).render(
-//     <DataProvider>
-//     <App />
-//     </DataProvider>
-// )
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <DataProvider>
       <App />
     </DataProvider>
-  </BrowserRouter>,
-    document.getElementById("root")
-  );
+  </BrowserRouter>
+);
